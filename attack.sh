4c8d4d723c6e3112b83ad7c37a6fdcada3ebf1c9
Jimmy <jabez007@users.noreply.github.com>
2017-07-11 19:54:10 -0500

    attack all users in shadow file

--- attack.sh
+++ attack.sh
@@ -3,83 +3,89 @@
 # Set the internal field separator
 IFS=$'\n'
 
-if [ -z "$3" ]
+if [ -z "$1" ]
 then
-        shadow_file="/etc/shadow"
+	printf "No dictionary file given\n"
+	exit 1
 else
-        if [ -r "$3" ]
+        if [ -r "$1" ]
         then
-                shadow_file="$3"
+                dictionary_file="$1"
         else
-                echo "Bad shadow file: $3"
+                echo "Bad dictionary file: $1"
                 exit 1
         fi
 fi
 
-if [ $(cat "$shadow_file" | grep "$1:") ]
+if [ -z "$2" ]
 then
-        echo "Username $1 is good"
-        pass_hash=$(cat "$shadow_file" | grep "$1:" | cut -d ":" -f 2)
-        
-        algo_id=$(echo "$pass_hash" | cut -d "$" -f 2)
-        if [ "$algo_id" == "1" ]
-        then
-                hash_algo="md5"
-        fi
-        if [ "$algo_id" == "2a" ] || [ "$algo_id" == "2y" ]
-        then
-                hash_algo="blowfish"
-        fi
-        if [ "$algo_id" == "5" ]
-        then
-                hash_algo="sha-256"
-        fi
-        if [ "$algo_id" == "6" ]
-        then
-                hash_algo="sha-512"
-        fi
-        echo "hashing algorithm: $hash_algo"
-        
-        salt=$(echo "$pass_hash" | cut -d "$" -f 3)
-        echo "password hash salt: $salt"
+	shadow_file="/etc/shadow"
+else
+	if [ -r "$2" ]
+	then
+		shadow_file="$2"
+	else
+		echo "Bad shadow file: $2"
+		exit 1
+	fi
+fi
 
-        if [ -z "$2" ]
+printf "Attacking %s using %s\n" "$shadow_file" "$dictionary_file"
+
+
+while read -r line
+do
+	test_pass="$(printf "%s" "$line" | cut -d ":" -f 2)"
+	if [ ${#test_pass} -gt 3 ]
         then
-                usr_pass=$(mkpasswd -s -m "$hash_algo" -S "$salt")
-                if [ "$usr_pass" == "$pass_hash" ]
-                then
-                        echo "Password is good"
-                else
-                        echo "Password is bad"
-                fi
-                exit 0
-        fi
-        
-        if [ -r "$2" ]
-        then
-                for pass in $(cat "$2")
+		echo "=========================="
+
+                username=$(printf "%s" "$line" | cut -d ":" -f 1)
+                printf "Username: %s\n" "$username"
+
+                pass_hash=$(printf "%s" "$line" | cut -d ":" -f 2)
+                #printf "%s\n" "$password"
+
+		unset hash_algo
+		algo_id=$(printf "%s" "$pass_hash" | cut -d "$" -f 2)
+		if [ "$algo_id" == "1" ]
+	        then
+        	        hash_algo="md5"
+        	fi
+        	if [ "$algo_id" == "2a" ] || [ "$algo_id" == "2y" ]
+        	then
+                	hash_algo="blowfish"
+        	fi
+        	if [ "$algo_id" == "5" ]
+        	then
+                	hash_algo="sha-256"
+        	fi
+        	if [ "$algo_id" == "6" ]
+        	then
+                	hash_algo="sha-512"
+        	fi
+        	printf "Hashing algorithm: %s\n" "$hash_algo"
+
+                salt=$(printf "%s" "$pass_hash" | cut -d "$" -f 3)
+                #printf "%s\n" "$salt"
+
+		for word in $(cat "$dictionary_file")
                 do
-                        if [ "$(echo -n "$pass" | mkpasswd -s -m "$hash_algo" -S "$salt")" == "$pass_hash" ]
+                        if [ "$(echo -n "$word" | mkpasswd -s -m "$hash_algo" -S "$salt")" == "$pass_hash" ]
                         then
-                                echo "Password $pass is good"
-                                exit 0
+				printf "\nFound password: %s\n" "$word"
+                		echo "=========================="
+                		break
                         else
-                                echo "Password $pass is bad"
+                                printf "."
                         fi
                 done
-        else
-                if [ "$(echo -n "$2" | mkpasswd -s -m "$hash_algo" -S "$salt")" == "$pass_hash" ]
-                then
-                        echo "Password $2 is good"
-                else
-                        echo "Password $2 is bad"
-                fi
+
         fi
 
-else
-        echo "Bad username: $1"
-fi
+done < "$shadow_file"
 
 unset IFS
 
+printf "\n"
 exit 0
